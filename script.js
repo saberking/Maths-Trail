1f24fd5329404546adbe52e3bce3ab6db39f765b
saber king <saberking321@gmail.com>
2020-05-13 01:21:38 +0100

    siz

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ var pages =[
 
 var currentPage=0;
 function requestValue(min,max){
-    get("text").innerHTML+="<input type='number' min='"+min+"' max='"+max+"' id='input' value='0' style='font-size:100px'></input>";
+    get("text").innerHTML+="<input type='number' min='"+min+"' max='"+max+"' id='input' value='0' style='font-size:100px' height='100px' width='400px'></input>";
 }
 
 function enterValue(name){
